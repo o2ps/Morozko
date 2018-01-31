44238e4a54b571128813d59ef36d2abad7fb102e
Jiří Pudil <me@jiripudil.cz>
2018-01-31 09:37:11 +0100

    DI CacheWarmer: log debugging info

--- src/CacheWarmers/NetteConfiguratorCacheWarmer.php
+++ src/CacheWarmers/NetteConfiguratorCacheWarmer.php
@@ -7,13 +7,19 @@ namespace Oops\Morozko\CacheWarmers;
 use Oops\Morozko\CacheWarmer;
 use Oops\Morozko\CacheWarmupFailedException;
 use PackageVersions\Versions;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\NullLogger;
 use Symfony\Component\Process\Exception\RuntimeException;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
 
 
-final class NetteConfiguratorCacheWarmer implements CacheWarmer
+final class NetteConfiguratorCacheWarmer implements CacheWarmer, LoggerAwareInterface
 {
+
+	use LoggerAwareTrait;
+
 
 	/**
 	 * @var string
@@ -24,6 +30,7 @@ final class NetteConfiguratorCacheWarmer implements CacheWarmer
 	public function __construct(string $wwwDir)
 	{
 		$this->wwwDir = $wwwDir;
+		$this->logger = new NullLogger();
 	}
 
 
@@ -37,12 +44,17 @@ final class NetteConfiguratorCacheWarmer implements CacheWarmer
 
 	private function findAvailablePort(): int
 	{
+		$this->logger->info('Finding available port.');
+
 		for ($port = 8000; $port < 9000; $port++) {
+			$this->logger->debug(\sprintf('Trying port %d.', $port));
 			$socket = @\fsockopen('127.0.0.1', $port);
 			if ($socket === FALSE) {
+				$this->logger->debug(\sprintf('Port %d is available.', $port));
 				return $port;
 			}
 
+			$this->logger->debug(\sprintf('Port %d is unavailable.', $port));
 			\fclose($socket);
 		}
 
@@ -54,12 +66,15 @@ final class NetteConfiguratorCacheWarmer implements CacheWarmer
 
 	private function runWebServer(int $port): Process
 	{
+		$this->logger->info('Locating PHP executable.');
 		$phpExecutable = (new PhpExecutableFinder())->find();
 		if ($phpExecutable === FALSE) {
 			throw new CacheWarmupFailedException(
 				'Cannot launch PHP built-in webserver: cannot find PHP executable'
 			);
 		}
+
+		$this->logger->debug(\sprintf('PHP executable found at %s.', $phpExecutable));
 
 		try {
 			$webServer = new Process(\sprintf(
@@ -69,22 +84,41 @@ final class NetteConfiguratorCacheWarmer implements CacheWarmer
 				\escapeshellarg($this->wwwDir)
 			));
 
+			$this->logger->info(\sprintf('Starting PHP web-server process on port %d.', $port));
 			$webServer->start();
 
-			// wait for the server to start listening
 			$start = \microtime(TRUE);
+			$attempt = 1;
+			$this->logger->debug(\sprintf('Waiting for PHP web-server to start listening on port %d.', $port));
 			do {
-				if (\microtime(TRUE) - $start > 5) {
+				$timeTaken = \microtime(TRUE) - $start;
+				if ($timeTaken > 5) {
 					throw new CacheWarmupFailedException(
 						\sprintf(
-							'Waiting for PHP web-server to listen on %d timed out after 5 seconds, giving up.',
-							$port
+							'PHP web-server failed to start listening on port %d in %d attempts within 5 seconds, giving up.',
+							$port,
+							$attempt
 						)
 					);
 				}
 
 				\usleep(200 * 1000); // 200 ms
+				\error_clear_last();
 				$socket = @\fsockopen('127.0.0.1', $port);
+
+				if ($socket === FALSE) {
+					$error = \error_get_last();
+					$this->logger->debug(\sprintf(
+						'Cannot connect to 127.0.0.1:%d in attempt %d after %.3f seconds%s',
+						$port,
+						$attempt,
+						$timeTaken,
+						$error !== NULL ? \sprintf(': %s', \json_encode($error)) : '.'
+					));
+				}
+
+				$attempt++;
+
 			} while ($socket === FALSE);
 			\fclose($socket);
 
@@ -105,6 +139,8 @@ final class NetteConfiguratorCacheWarmer implements CacheWarmer
 
 	private function dispatchRequest(int $port, Process $webServer): void
 	{
+		$this->logger->info(\sprintf('Dispatching an HTTP request to port %d.', $port));
+
 		$curl = \curl_init(\sprintf('http://127.0.0.1:%d', $port));
 		\curl_setopt($curl, \CURLOPT_FAILONERROR, TRUE);
 		\curl_setopt($curl, \CURLOPT_RETURNTRANSFER, TRUE);
@@ -117,6 +153,12 @@ final class NetteConfiguratorCacheWarmer implements CacheWarmer
 		));
 
 		\curl_exec($curl);
+
+		$httpCode = \curl_getinfo($curl, \CURLINFO_RESPONSE_CODE);
+		$this->logger->debug(\sprintf(
+			'HTTP request to the application returned status code %d',
+			$httpCode
+		));
 
 		try {
 			if (\curl_errno($curl) !== 0) {
